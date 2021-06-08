9e0a8cb4823e74e452a97deadbb779d565d8810d
Akshay Kumar Tripathi <50769001+AkshayKumarTripathi@users.noreply.github.com>
2021-06-08 19:01:36 +0530

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -2,7 +2,7 @@
 
 
 def start():
-#This function will have the baord and we will see if any solution is possible or not
+#This function will have the board and we will see if any solution is possible or not
     
     sudoku=[[0,2,6,0,0,0,8,1,0],[3,0,0,7,0,8,0,0,6],[4,0,0,0,5,0,0,0,7],[0,5,0,1,0,7,0,9,0],[0,0,3,9,0,5,1,0,0],[0,4,0,3,0,2,0,5,0],[1,0,0,0,3,0,0,0,2],[5,0,0,2,0,4,0,0,9],[0,3,8,0,0,0,4,6,0]]
     printoi(sudoku)
